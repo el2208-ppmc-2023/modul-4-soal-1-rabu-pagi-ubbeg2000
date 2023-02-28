886c19ce6dab3a9d9c45cd26b9ca3c229b844302
Elkhan <74042126+ubbeg2000@users.noreply.github.com>
2023-02-28 22:16:08 +0700

    Update main.c

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #define MAX_LEN 100
 
@@ -15,52 +17,141 @@ typedef struct Pokemon {
    char id[MAX_LEN];
    char name[MAX_LEN];
    char type[MAX_LEN];
-} Pokemon;
+}Pokemon;
 
 //Fungsi untuk melakukan print sesuai input structure
 void printPokemon(Pokemon *p) {
-   ...
+   p->name[0] = toupper(p->name[0]); //uppercase letter pertama
+   p->type[0] = toupper(p->type[0]); 
+   for (int i = 1; i<MAX_LEN; i++){ 
+      if (p->type[i] == ','){
+         p->type[i+1] = toupper(p->type[i+1]);
+      }
+   }
 
-   printf("\nID: %s\nNama: %s\nType: %s\n", ..., ..., ...);
+   printf("\nID: %s\nNama: %s\nType: %s\n", p->id, p->name, p->type);
 }
 
 //Fungsi untuk melakukan pencarian ID pada array structure
-void searchID(Pokemon *p, int n, int ID) {
-   ...
+void searchID(Pokemon *p[], int n, int ID) {
+   int i, found = 0;
+   char *ID_str;
+   char *temp;
+   sprintf(ID_str, "%d", ID);
+   
+   for (i = 0; i < n; i++) {
+      temp = strstr(p[i]->id, ID_str); //cari needle di haystack
+      if (temp!=NULL) {
+         found = 1;
+         printPokemon(p[i]);
+      }
+   }
 
-   printf("\nPokemon yang memiliki ID %d belum ditangkap!", ...);
+   if (!found) {
+      printf("\nPokemon yang memiliki ID %d belum ditangkap!", ID);
+   }
 }
 
 //Fungsi untuk melakukan pencarian name pada array structure
-void searchName(Pokemon *p, int n, char name[]) {
-   ...
+void searchName(Pokemon *p[], int n, char name[]) {
+   int i, found = 0;
+   char *temp;
+    
+   for (i = 0; i < n; i++) {
+      temp = strstr(p[i]->name, name); //cari needle di haystack
+      if (temp!=NULL) {
+         found = 1;
+         printPokemon(p[i]);
+      }
+   }
 
-   printf("\nPokemon dengan inisial nama %s belum ditangkap!", ...);
+   if (!found) {
+      printf("\nPokemon dengan inisial nama %s belum ditangkap!", name);
+   }
 }
 
 //Fungsi untuk melakukan pencarian type pada array structure
-void searchType(Pokemon *p, int n, char type[]) {
-   ...
+void searchType(Pokemon *p[], int n, char type[]) {
+   int i, found = 0;
+   char *temp;
 
-   printf("\nPokemon dengan type %s belum ditangkap!", ...);
+   for (i = 0; i < n; i++) {
+      temp = strstr(p[i]->type, type); //cari needle di haystack
+      if (temp!=NULL) {
+         found = 1;
+         printPokemon(p[i]);
+      }
+   }
+
+   if (!found) {
+      printf("\nPokemon dengan type %s belum ditangkap!", type);
+   }
 }
 
 int main() {
-    
-    printf("Masukkan nama file input: ");
+   char name[20], type[20];
+   int ID_int;
    
-    //Inisiasi array dinamis structure
-    Pokemon ... = ...;
+   char filename[MAX_LEN];
+   printf("Masukkan nama file input: ");
+   scanf("%s", &filename);
+   FILE* fp = fopen(filename, "r");
+   if (fp == NULL){
+      printf("\nError: file tidak tersedia!\n");
+      return 0;
+   }
 
+   char line[MAX_LEN];
+   int len=(-1);
+   while (fgets(line, MAX_LEN, fp)) { //hitung jumlah baris
+      len++;
+   }
+   rewind(fp); //atur posisi file ke awal lagi
+   
+   //inisiasi array dinamis
+   Pokemon **pokedex = malloc(len * sizeof(Pokemon*));
+   for (int i = 0; i < len; i++) {
+      pokedex[i] = malloc(sizeof(Pokemon));
+   } 
 
-    printf("\nProgram mencari ID pokemon\nMasukkan ID pokemon yang ingin dicari: ");
-    searchID(..., ..., ...);
+   char* token; 
+   int i =(-1);
+   int input;
+   
+   while (fgets(line, MAX_LEN, fp)) {
+      if (i==(-1)){ //ambil input
+         token = strtok(line, "\n");
+         input = atoi(token);        
+         i++;
+      }
+      else{ //ambil data pokemon
+         token = strtok(line, ";");
+         strcpy(pokedex[i]->id, token);
+         token = strtok(NULL, ";");
+         strcpy(pokedex[i]->name, strlwr(token));
+         token = strtok(NULL, "\n");
+         strcpy(pokedex[i]->type, strlwr(token));
+         i++;
+      }
+   }
 
-    printf("\nProgram mencari Nama pokemon\nMasukkan Nama pokemon yang ingin dicari: ");
-    searchName(..., ..., ...);
+   if (input == 1){
+      printf("\nProgram mencari ID pokemon\nMasukkan ID pokemon yang ingin dicari: ");
+      scanf("%d", &ID_int);
+      searchID(pokedex, len, ID_int);
+   }
+   else if (input == 2){
+      printf("\nProgram mencari Nama pokemon\nMasukkan Nama pokemon yang ingin dicari: ");
+      scanf("%s", &name);
+      searchName(pokedex, len, strlwr(name));
+   }
+   else if (input == 3){
+      printf("\nProgram mencari Type pokemon\nMasukkan Type pokemon yang ingin dicari: ");
+      scanf("%s", &type);
+      searchType(pokedex, len, strlwr(type));
+   }
 
-    printf("\nProgram mencari Type pokemon\nMasukkan Type pokemon yang ingin dicari: ");
-    searchType(..., ..., ...);
-
-    return 0;
+   free(pokedex);
+   fclose(fp);
+   return 0;
 }
